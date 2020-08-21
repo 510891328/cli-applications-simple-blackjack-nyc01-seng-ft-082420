5b4421c868aef1d7930992a70b37552f80dbb813
Yunhao Gao <as510891328@gmail.com>
2020-08-21 04:13:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,16 +6,16 @@ def deal_card
   rand(1..11)
 end
 
-def display_card_total(card)
+def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  puts "Type 'h' to hi or 's' to stay"
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  gets
+  gets.chomp
 end
 
 def end_game(num)
